d07a888bbf72db704f6be6bcaa3f8c692e04af0f
Joshua Stresing <pyrotorch69@yahoo.com>
2022-04-07 16:47:49 -0700

    moved main to views for easy points

--- src/Views/Main/Main.js
+++ src/Views/Main/Main.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import './Main.css';
 import logo192 from './logo192.png';
-import Toon from '../Toon/Toon.js';
-import Picker from '../Picker/Picker.js';
-import Slogan from '../Slogan/Slogan.js';
+import Toon from '../../components/Toon/Toon.js';
+import Picker from '../../components/Picker/Picker.js';
+import Slogan from '../../components/Slogan/Slogan.js';
 
 
 export default function Main() {
